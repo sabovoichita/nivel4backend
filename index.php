c50017674da26ef9f0ef4bebc10e1706b89d21b2
Voichita Maria <sabo_voichita@yahoo.com>
2025-10-30 10:50:24 +0100

    LENG4_U1_1_Introduccion_a_PHP

--- index.php
+++ index.php
@@ -11,5 +11,20 @@
   <p>La fecha y hora actual es:
     <?php echo date("Y-m-d H:i:s"); ?>
   </p>
+  <!-- <p><?php phpinfo(); ?></p> -->
+   
+  <?php
+echo '¡Hola, mundo!';
+print 'Bienvenido a PHP'?>
+
+<p>La hora actual es: <?php echo date('H:i:s'); ?></p>
+
+<h3>Lista echa con PHP:</h3>
+<ul>
+<?php for ($i = 1; $i <= 5; $i++): ?>
+ <li>Elemento <?php echo $i; ?></1i>
+<?php endfor; ?>
+</ul>
+
 </body>
 </html>
