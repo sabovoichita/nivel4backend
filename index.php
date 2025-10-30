c083d9d1e5d7e35c9ec314f4cdf36892d005b581
Voichita Maria <sabo_voichita@yahoo.com>
2025-10-30 10:32:25 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Mi página PHP</title>
+</head>
+<body>
+  <h1>Bienvenido a mi página</h1>
+
+  <p>La fecha y hora actual es:
+    <?php echo date("Y-m-d H:i:s"); ?>
+  </p>
+</body>
+</html>
